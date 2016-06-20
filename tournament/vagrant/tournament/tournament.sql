8c6e06dd6937d81bb78e6a46d89132b70a5e8a95
Wenchen Li <wenchen.li.cs@gmail.com>
2016-06-20 12:28:38 -0400

    finish backend part for the tournament schedule with psql

---
+++ tournament/vagrant/tournament/tournament.sql
@@ -0,0 +1,18 @@
+-- Table definitions for the tournament project.
+--
+-- Put your SQL 'create table' statements in this file; also 'create view'
+-- statements if you choose to use it.
+--
+-- You can write comments in this file by starting them with two dashes, like
+-- these lines here.
+
+
+
+CREATE TABLE players ( player_id SERIAL,
+                       player_name TEXT
+                      );
+
+CREATE TABLE matches ( matches_id SERIAL,
+                       winner TEXT,
+                       loser TEXT
+                      );
