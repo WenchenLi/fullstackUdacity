2e767dddbf74f685745fca089c1bee9c2d612756
Wenchen Li <wenchen.li.cs@gmail.com>
2016-06-21 19:38:50 -0400

    refactoring to catalog,item framework tests all functionality

--- item-catalog/vagrant/catalog/database_setup_item_catalog.py
+++ item-catalog/vagrant/catalog/database_setup_item_catalog.py
@@ -1,5 +1,5 @@
 """
-script to setup resturant, menuitem database using sqlachememy
+script to setup item catalog database using sqlachememy
 
 """
 from sqlalchemy import Column, ForeignKey, Integer, String
@@ -18,9 +18,9 @@ class User(Base):
     email = Column(String(250), nullable=False)
     picture = Column(String(250))
 
-class Restaurant(Base):
-    """restaurant entity"""
-    __tablename__ = 'restaurant'
+class Catalog(Base):
+    """Catalog entity"""
+    __tablename__ = 'catalog'
 
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
@@ -36,17 +36,15 @@ class Restaurant(Base):
         }
 
 
-class MenuItem(Base):
-    """MenuItem in the resturant"""
-    __tablename__ = 'menu_item'
+class Item(Base):
+    """Item entity under Catalog"""
+    __tablename__ = 'item'
 
     name = Column(String(80), nullable=False)
     id = Column(Integer, primary_key=True)
     description = Column(String(250))
-    price = Column(String(8))
-    course = Column(String(250))
-    restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
-    restaurant = relationship(Restaurant)
+    catalog_id = Column(Integer, ForeignKey('catalog.id'))
+    catalog = relationship(Catalog)
     user_id = Column(Integer, ForeignKey('user.id'))
     user = relationship(User)
 
@@ -57,12 +55,9 @@ class MenuItem(Base):
             'name': self.name,
             'description': self.description,
             'id': self.id,
-            'price': self.price,
-            'course': self.course,
         }
 
-
-engine = create_engine('sqlite:///restaurantmenuwithusers.db')
+engine = create_engine('sqlite:///catalogitemwithusers.db')
 
 
 Base.metadata.create_all(engine)
--- item-catalog/vagrant/catalog/item-catalog.py
+++ item-catalog/vagrant/catalog/item-catalog.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, jsonify, url_for, flash
 from sqlalchemy import create_engine, asc
 from sqlalchemy.orm import sessionmaker
-from database_setup import Base, Restaurant, MenuItem, User
+from database_setup_item_catalog import Base, Catalog, Item, User
 from flask import session as login_session
 import random
 import string
@@ -16,11 +16,11 @@ app = Flask(__name__)
 
 CLIENT_ID = json.loads(
     open('g_client_secrets.json', 'r').read())['web']['client_id']
-APPLICATION_NAME = "Restaurant Menu Application"
+APPLICATION_NAME = "Item Catalog Application"
 
 
 # Connect to Database and create database session
-engine = create_engine('sqlite:///restaurantmenuwithusers.db')
+engine = create_engine('sqlite:///catalogitemwithusers.db')
 Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
@@ -205,8 +205,6 @@ def gconnect():
     return output
 
 # User Helper Functions
-
-
 def createUser(login_session):
     newUser = User(name=login_session['username'], email=login_session[
                    'email'], picture=login_session['picture'])
@@ -217,6 +215,7 @@ def createUser(login_session):
 
 
 def getUserInfo(user_id):
+    print user_id
     user = session.query(User).filter_by(id=user_id).one()
     return user
 
@@ -252,169 +251,166 @@ def gdisconnect():
         return response
 
 
-# JSON APIs to view Restaurant Information
-@app.route('/restaurant/<int:restaurant_id>/menu/JSON')
-def restaurantMenuJSON(restaurant_id):
-    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
-    items = session.query(MenuItem).filter_by(
-        restaurant_id=restaurant_id).all()
-    return jsonify(MenuItems=[i.serialize for i in items])
+# JSON APIs to view Catalog Information
+@app.route('/catalog/<int:catalog_id>/item/JSON')
+def catalogItemJSON(catalog_id):
+    catalog = session.query(Catalog).filter_by(id=catalog_id).one()
+    items = session.query(ItemItem).filter_by(
+        catalog_id=catalog_id).all()
+    return jsonify(ItemItems=[i.serialize for i in items])
 
 
-@app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/JSON')
-def menuItemJSON(restaurant_id, menu_id):
-    Menu_Item = session.query(MenuItem).filter_by(id=menu_id).one()
-    return jsonify(Menu_Item=Menu_Item.serialize)
+@app.route('/catalog/<int:catalog_id>/item/<int:item_id>/JSON')
+def itemItemJSON(catalog_id, item_id):
+    Item_Item = session.query(ItemItem).filter_by(id=item_id).one()
+    return jsonify(Item_Item=Item_Item.serialize)
 
 
-@app.route('/restaurant/JSON')
-def restaurantsJSON():
-    restaurants = session.query(Restaurant).all()
-    return jsonify(restaurants=[r.serialize for r in restaurants])
+@app.route('/catalog/JSON')
+def catalogsJSON():
+    catalogs = session.query(Catalog).all()
+    return jsonify(catalogs=[r.serialize for r in catalogs])
 
 
-# Show all restaurants
+# Show all catalogs
 @app.route('/')
-@app.route('/restaurant/')
-def showRestaurants():
-    restaurants = session.query(Restaurant).order_by(asc(Restaurant.name))
+@app.route('/catalog/')
+def showCatalogs():
+    catalogs = session.query(Catalog).order_by(asc(Catalog.name))
     if 'username' not in login_session:
-        return render_template('publicrestaurants.html', restaurants=restaurants)
+        return render_template('publiccatalogs.html', catalogs=catalogs)
     else:
-        return render_template('restaurants.html', restaurants=restaurants)
+        return render_template('catalogs.html', catalogs=catalogs)
 
-# Create a new restaurant
+# Create a new catalog
 
 
-@app.route('/restaurant/new/', methods=['GET', 'POST'])
-def newRestaurant():
+@app.route('/catalog/new/', methods=['GET', 'POST'])
+def newCatalog():
     if 'username' not in login_session:
         return redirect('/login')
     if request.method == 'POST':
-        newRestaurant = Restaurant(
+        newCatalog = Catalog(
             name=request.form['name'], user_id=login_session['user_id'])
-        session.add(newRestaurant)
-        flash('New Restaurant %s Successfully Created' % newRestaurant.name)
+        session.add(newCatalog)
         session.commit()
-        return redirect(url_for('showRestaurants'))
+        flash('New Catalog %s Successfully Created' % newCatalog.name)
+        return redirect(url_for('showCatalogs'))
     else:
-        return render_template('newRestaurant.html')
+        return render_template('newCatalog.html')
 
-# Edit a restaurant
+# Edit a catalog
 
 
-@app.route('/restaurant/<int:restaurant_id>/edit/', methods=['GET', 'POST'])
-def editRestaurant(restaurant_id):
-    editedRestaurant = session.query(
-        Restaurant).filter_by(id=restaurant_id).one()
+@app.route('/catalog/<int:catalog_id>/edit/', methods=['GET', 'POST'])
+def editCatalog(catalog_id):
+    editedCatalog = session.query(
+        Catalog).filter_by(id=catalog_id).one()
     if 'username' not in login_session:
         return redirect('/login')
-    if editedRestaurant.user_id != login_session['user_id']:
-        return "<script>function myFunction() {alert('You are not authorized to edit this restaurant. Please create your own restaurant in order to edit.');}</script><body onload='myFunction()''>"
+    if editedCatalog.user_id != login_session['user_id']:
+        return "<script>function myFunction() {alert('You are not authorized to edit this catalog. Please create your own catalog in order to edit.');}</script><body onload='myFunction()''>"
     if request.method == 'POST':
         if request.form['name']:
-            editedRestaurant.name = request.form['name']
-            flash('Restaurant Successfully Edited %s' % editedRestaurant.name)
-            return redirect(url_for('showRestaurants'))
+            editedCatalog.name = request.form['name']
+            flash('Catalog Successfully Edited %s' % editedCatalog.name)
+            return redirect(url_for('showCatalogs'))
     else:
-        return render_template('editRestaurant.html', restaurant=editedRestaurant)
+        return render_template('editCatalog.html', catalog=editedCatalog)
 
 
-# Delete a restaurant
-@app.route('/restaurant/<int:restaurant_id>/delete/', methods=['GET', 'POST'])
-def deleteRestaurant(restaurant_id):
-    restaurantToDelete = session.query(
-        Restaurant).filter_by(id=restaurant_id).one()
+# Delete a catalog
+@app.route('/catalog/<int:catalog_id>/delete/', methods=['GET', 'POST'])
+def deleteCatalog(catalog_id):
+    catalogToDelete = session.query(
+        Catalog).filter_by(id=catalog_id).one()
     if 'username' not in login_session:
         return redirect('/login')
-    if restaurantToDelete.user_id != login_session['user_id']:
-        return "<script>function myFunction() {alert('You are not authorized to delete this restaurant. Please create your own restaurant in order to delete.');}</script><body onload='myFunction()''>"
+    if catalogToDelete.user_id != login_session['user_id']:
+        return "<script>function myFunction() {alert('You are not authorized to delete this catalog. Please create your own catalog in order to delete.');}</script><body onload='myFunction()''>"
     if request.method == 'POST':
-        session.delete(restaurantToDelete)
-        flash('%s Successfully Deleted' % restaurantToDelete.name)
+        session.delete(catalogToDelete)
+        flash('%s Successfully Deleted' % catalogToDelete.name)
         session.commit()
-        return redirect(url_for('showRestaurants', restaurant_id=restaurant_id))
+        return redirect(url_for('showCatalogs', catalog_id=catalog_id))
     else:
-        return render_template('deleteRestaurant.html', restaurant=restaurantToDelete)
+        return render_template('deleteCatalog.html', catalog=catalogToDelete)
 
-# Show a restaurant menu
+# Show a catalog item
 
 
-@app.route('/restaurant/<int:restaurant_id>/')
-@app.route('/restaurant/<int:restaurant_id>/menu/')
-def showMenu(restaurant_id):
-    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
-    creator = getUserInfo(restaurant.user_id)
-    items = session.query(MenuItem).filter_by(
-        restaurant_id=restaurant_id).all()
+@app.route('/catalog/<int:catalog_id>/')
+@app.route('/catalog/<int:catalog_id>/item/')
+def showItem(catalog_id):
+    catalog = session.query(Catalog).filter_by(id=catalog_id).one()
+    creator = getUserInfo(catalog.user_id)
+    items = session.query(Item).filter_by(
+        catalog_id=catalog_id).all()
     if 'username' not in login_session or creator.id != login_session['user_id']:
-        return render_template('publicmenu.html', items=items, restaurant=restaurant, creator=creator)
+        return render_template('publicitem.html', items=items, catalog=catalog, creator=creator)
     else:
-        return render_template('menu.html', items=items, restaurant=restaurant, creator=creator)
+        return render_template('item.html', items=items, catalog=catalog, creator=creator)
 
 
-# Create a new menu item
-@app.route('/restaurant/<int:restaurant_id>/menu/new/', methods=['GET', 'POST'])
-def newMenuItem(restaurant_id):
+# Create a new item item
+@app.route('/catalog/<int:catalog_id>/item/new/', methods=['GET', 'POST'])
+def newItem(catalog_id):
     if 'username' not in login_session:
         return redirect('/login')
-    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
-    if login_session['user_id'] != restaurant.user_id:
-        return "<script>function myFunction() {alert('You are not authorized to add menu items to this restaurant. Please create your own restaurant in order to add items.');}</script><body onload='myFunction()''>"
-        if request.method == 'POST':
-            newItem = MenuItem(name=request.form['name'], description=request.form['description'], price=request.form[
-                               'price'], course=request.form['course'], restaurant_id=restaurant_id, user_id=restaurant.user_id)
-            session.add(newItem)
-            session.commit()
-            flash('New Menu %s Item Successfully Created' % (newItem.name))
-            return redirect(url_for('showMenu', restaurant_id=restaurant_id))
+    catalog = session.query(Catalog).filter_by(id=catalog_id).one()
+    if login_session['user_id'] != catalog.user_id:
+        return "<script>function myFunction() {alert('You are not authorized to add item items to this catalog. Please create your own catalog in order to add items.');}</script><body onload='myFunction()''>"
+    if request.method == 'POST':
+        newItem = Item(name=request.form['name'], description=request.form['description'],
+                       catalog_id=catalog_id, user_id=login_session['user_id'])
+        session.add(newItem)
+        session.commit()
+        flash('New Item %s Item Successfully Created' % (newItem.name))
+        return redirect(url_for('showItem', catalog_id=catalog_id))
     else:
-        return render_template('newmenuitem.html', restaurant_id=restaurant_id)
+        return render_template('newItem.html', catalog_id=catalog_id)
 
-# Edit a menu item
-
-
-@app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/edit', methods=['GET', 'POST'])
-def editMenuItem(restaurant_id, menu_id):
+# Edit a item item
+@app.route('/catalog/<int:catalog_id>/item/<int:item_id>/edit', methods=['GET', 'POST'])
+def editItem(catalog_id, item_id):
     if 'username' not in login_session:
         return redirect('/login')
-    editedItem = session.query(MenuItem).filter_by(id=menu_id).one()
-    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
-    if login_session['user_id'] != restaurant.user_id:
-        return "<script>function myFunction() {alert('You are not authorized to edit menu items to this restaurant. Please create your own restaurant in order to edit items.');}</script><body onload='myFunction()''>"
+    editedItem = session.query(Item).filter_by(id=item_id).one()
+    catalog = session.query(Catalog).filter_by(id=catalog_id).one()
+    if login_session['user_id'] != catalog.user_id:
+        return "<script>function myFunction() {alert('You are not authorized to edit item items to this catalog. Please create your own catalog in order to edit items.');}</script><body onload='myFunction()''>"
     if request.method == 'POST':
         if request.form['name']:
             editedItem.name = request.form['name']
         if request.form['description']:
             editedItem.description = request.form['description']
-        if request.form['price']:
-            editedItem.price = request.form['price']
-        if request.form['course']:
-            editedItem.course = request.form['course']
+
         session.add(editedItem)
         session.commit()
-        flash('Menu Item Successfully Edited')
-        return redirect(url_for('showMenu', restaurant_id=restaurant_id))
+        flash('Item Successfully Edited')
+        return redirect(url_for('showItem', catalog_id=catalog_id))
     else:
-        return render_template('editmenuitem.html', restaurant_id=restaurant_id, menu_id=menu_id, item=editedItem)
+        return render_template('edititem.html',
+            catalog_id=catalog_id, item_id=item_id, item=editedItem)
 
 
-# Delete a menu item
-@app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/delete', methods=['GET', 'POST'])
-def deleteMenuItem(restaurant_id, menu_id):
+# Delete a item item
+@app.route('/catalog/<int:catalog_id>/item/<int:item_id>/delete',
+                methods=['GET', 'POST'])
+def deleteItem(catalog_id, item_id):
     if 'username' not in login_session:
         return redirect('/login')
-    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
-    itemToDelete = session.query(MenuItem).filter_by(id=menu_id).one()
-    if login_session['user_id'] != restaurant.user_id:
-        return "<script>function myFunction() {alert('You are not authorized to delete menu items to this restaurant. Please create your own restaurant in order to delete items.');}</script><body onload='myFunction()''>"
+    catalog = session.query(Catalog).filter_by(id=catalog_id).one()
+    itemToDelete = session.query(Item).filter_by(id=item_id).one()
+    if login_session['user_id'] != catalog.user_id:
+        return "<script>function myFunction() {alert('You are not authorized to delete item items to this catalog. Please create your own catalog in order to delete items.');}</script><body onload='myFunction()''>"
     if request.method == 'POST':
         session.delete(itemToDelete)
         session.commit()
-        flash('Menu Item Successfully Deleted')
-        return redirect(url_for('showMenu', restaurant_id=restaurant_id))
+        flash('Item Successfully Deleted')
+        return redirect(url_for('showItem', catalog_id=catalog_id))
     else:
-        return render_template('deleteMenuItem.html', item=itemToDelete)
+        return render_template('deleteitem.html', item=itemToDelete)
 
 
 # Disconnect based on provider
@@ -435,10 +431,10 @@ def disconnect():
         del login_session['user_id']
         del login_session['provider']
         flash("You have successfully been logged out.")
-        return redirect(url_for('showRestaurants'))
+        return redirect(url_for('showCatalogs'))
     else:
         flash("You were not logged in")
-        return redirect(url_for('showRestaurants'))
+        return redirect(url_for('showCatalogs'))
 
 
 if __name__ == '__main__':
